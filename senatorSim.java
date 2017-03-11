5b3ee5d6e6d2dd9de09fc1769f9f6d96b0a9c7d1
slobanwala1 <slobanwala@gmail.com>
2017-03-11 04:25:03 -0600

    Added a few scanners for district testing

--- senatorSim.java
+++ senatorSim.java
@@ -9,8 +9,28 @@ public class SenatorSim
  
  public static void main( String[] args)
  {
+  int dType;
+  int flag = 0;
+  int gDecision = 0; //Decision that positiely affects
+  int bDecision = 0; //Decision that negatively affects
   SenatorSim simulation = new SenatorSim();
-  
- }
- 
+   System.out.println("Please enter the district you want from 1-3 or press 0 to exit.");
+   Scanner dScanner = new Scanner(System.in);
+   dType = dScanner.nextInt();
+   //For now there are only 3 districts.
+   if(dType > 0 && dType < 4)
+    {
+     District d = new District(dType);
+     //Here I set all the pop budget and death based on the dType, this is all done in the district class.
+     //For a bDecision you would make it a negative value as it adds the value onto the criteria.
+     //***We could just use a 1 or 0 and add or subtract a certain amount in the district but thats something I have to discuss
+     //***With the team.
+     d.setName(); d.setPop(0); d.setBudget(0); d.setDeath(0);
+     System.out.println(d.getDistrict()); 
+    }
+   if(dType == 0)
+    {
+    System.exit(0);
+    }
+  }
 }
