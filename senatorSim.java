f97bf38396c7cadbe1aebe5fe2032e6e713d27ab
Erheller <errheller@gmail.com>
2017-03-12 15:14:36 -0500

    Cleaning up the code

--- senatorSim.java
+++ senatorSim.java
@@ -11,31 +11,6 @@ public class SenatorSim
  {
      SenatorSim ss = new SenatorSim ();
      ss.processCommandLoop();
-     /*
-  int dType;
-  int flag = 0;
-  int gDecision = 0; //Decision that positiely affects
-  int bDecision = 0; //Decision that negatively affects
-  SenatorSim simulation = new SenatorSim();
-   System.out.println("Please enter the district you want from 1-3 or press 0 to exit.");
-   Scanner dScanner = new Scanner(System.in);
-   dType = dScanner.nextInt();
-   //For now there are only 3 districts.
-   if(dType > 0 && dType < 4)
-    {
-     District d = new District(dType);
-     //Here I set all the pop budget and death based on the dType, this is all done in the district class.
-     //For a bDecision you would make it a negative value as it adds the value onto the criteria.
-     //***We could just use a 1 or 0 and add or subtract a certain amount in the district but thats something I have to discuss
-     //***With the team.
-     d.setName(); d.setPop(0); d.setBudget(0); d.setDeath(0);
-     System.out.println(d.getDistrict()); 
-    }
-   if(dType == 0)
-    {
-    System.exit(0);
-    }
-      */
   }
    
     //have the user input a number
@@ -45,7 +20,6 @@ public class SenatorSim
     //exception handling
     int getDistrictFromUser () {
         System.out.println("Please enter type of district you want (1-3), or 0 to exit");
-        //Scanner tempScanner = new Scanner (System.in);
         int type = sc.nextInt();
         
         if (type == 0) {
@@ -63,6 +37,11 @@ public class SenatorSim
     }
     
     //create a district and initialize all of the fields
+    
+    //Here I set all the pop budget and death based on the dType, this is all done in the district class.
+    //For a bDecision you would make it a negative value as it adds the value onto the criteria.
+    //***We could just use a 1 or 0 and add or subtract a certain amount in the district but thats something I have to discuss
+    //***With the team.
     District createDistrict (int dType) {
         District d = new District(dType);
         d.setName();
