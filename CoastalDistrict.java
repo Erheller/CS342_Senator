0a6dae6f4364c813be75741a6ce66a9e625385df
Erheller <Erheller@10-107-210-90.vlan895.client.wireless.uic.edu>
2017-03-13 16:31:33 -0500

    Cleaning up code

--- CoastalDistrict.java
+++ CoastalDistrict.java
@@ -57,3 +57,131 @@ public class CoastalDistrict implements District {
       + "\nDeaths: " + Double.toString(death);
   }
 }
+
+
+
+
+
+
+class UrbanDistrict implements District {
+    
+    public int dType;
+    public double pop;
+    public double budget;
+    public double death;
+    public String dName;
+    public UrbanDistrict(int dType)
+    {
+        this.dType = dType;
+    }
+    //All the setters and getters for population
+    //***March 12, 2017 - Took out dType passing because we have classes for each district so dont need to specify here. - Shanil Lobanwala
+    public void setName()
+    {
+        dName = "Urban District";
+    }
+    public void setPop(double decision)
+    {
+        pop = 50000 + decision;
+    }
+    public void updatePop(double decision)
+    {
+        pop = decision;
+    }
+    public double getPop()
+    {
+        return pop;
+    }
+    public void setBudget(double decision)
+    {
+        budget = 200000 + decision;
+    }
+    public void updateBudget(double decision)
+    {
+        budget = decision;
+    }
+    public double getBudget()
+    {
+        return budget;
+    }
+    public void setDeath(double decision)
+    {
+        death = pop*.00823 + decision;
+    }
+    public void updateDeath(double decision)
+    {
+        death = decision;
+    }
+    public double getDeath()
+    {
+        return death;
+    }
+    public String getDistrict()
+    {
+        return "District Type: " + dName + "\nPopulation: " + Double.toString(pop) + "\nBudget: " + Double.toString(budget)
+        + "\nDeaths: " + Double.toString(death);
+    }
+}
+
+
+
+
+class RuralDistrict implements District {
+    
+    public int dType;
+    public double pop;
+    public double budget;
+    public double death;
+    public String dName;
+    public RuralDistrict(int dType)
+    {
+        this.dType = dType;
+    }
+    //All the setters and getters for population
+    //***March 12, 2017 - Took out dType passing because we have classes for each district so dont need to specify here. - Shanil Lobanwala
+    public void setName()
+    {
+        dName = "Rural District";
+    }
+    public void setPop(double decision)
+    {
+        pop = 2000 + decision;
+    }
+    public void updatePop(double decision)
+    {
+        pop = decision;
+    }
+    public double getPop()
+    {
+        return pop;
+    }
+    public void setBudget(double decision)
+    {
+        budget = 15000 + decision;
+    }
+    public void updateBudget(double decision)
+    {
+        budget = decision;
+    }
+    public double getBudget()
+    {
+        return budget;
+    }
+    public void setDeath(double decision)
+    {
+        death = pop*.00823 + decision;
+    }
+    public void updateDeath(double decision)
+    {
+        death = decision;
+    }
+    public double getDeath()
+    {
+        return death;
+    }
+    public String getDistrict()
+    {
+        return "District Type: " + dName + "\nPopulation: " + Double.toString(pop) + "\nBudget: " + Double.toString(budget)
+        + "\nDeaths: " + Double.toString(death);
+    }
+}
